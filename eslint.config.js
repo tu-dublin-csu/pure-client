3793806547686e86dfaef7522fc590330599a33c
Eoin Kilfeather <ekilfeather@gmail.com>
2025-01-09 11:44:59 +0000

    fix linter for node dev - wip #4

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,6 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.commonjs }},
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
 ];
