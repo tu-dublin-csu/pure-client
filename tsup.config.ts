556f13ba4935c983b5902086a5a44deb9db92763
cian-mcgovern <cian.mcgovern@tudublin.ie>
2025-02-19 13:35:19 +0000

    linking correct distribution files in package.json

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
     entry: ['src/index.ts'],
-    format: ['cjs', 'esm'], // Build for ESmodules
+    format: ['cjs', 'esm'], // Build for ESmodules and CommonJS
     dts: true, // Generate declaration file (.d.ts)
     splitting: false,
     sourcemap: true,
